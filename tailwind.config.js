21383c9e733f2af379b7e07d3b448103cd8f4764
koseckaya <kosetsaya@gmail.com>
2023-10-13 19:46:23 +0300

    feat: add mobile settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,33 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      inter: ['Inter', 'sans-serif']
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        sm: '24px',
+        '2xl': '40px',
+      },
+
+      screens: {
+        lg: '1024px',
+        xl: '1248px',
+        '2xl': '1440px',
+        
+      },
+    },
+    colors: {
+      secondary: '#41464C',
+      brand: '#1570EF',
+      primary: '#1C1F23',
+      success: '#12B76A',
+      error: '#F04438',
+      tertiary: '#6B7075',
+    }
+
   },
   variants: {},
   plugins: [],
